0d48c0ff3983bbd1f2ccce30964d7401c90610ab
nicai2333 <134794315+nicai2333@users.noreply.github.com>
2024-04-27 16:12:36 +0800

    test.h

--- test.h
+++ test.h
@@ -33,102 +33,102 @@ void random_string(u8 *s, size_t len);
 
 size_t random_number(void);
 
-// static const u8 ascii_table[16] = {
-//     '0', '1', '2', '3', '4', '5', '6', '7',
-//     '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
-// };
+static const u8 ascii_table[16] = {
+    '0', '1', '2', '3', '4', '5', '6', '7',
+    '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
+};
 
-// static const u8 inv_ascii_table[128] = {
-//     0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-//     0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-//     0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-//     0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-//     0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-//     0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-//        0,    1,    2,    3,    4,    5,    6,    7,
-//        8,    9, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-//     0xff,   10,   11,   12,   13,   14,   15, 0xff,
-//     0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-//     0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-//     0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-//     0xff,   10,   11,   12,   13,   14,   15, 0xff,
-//     0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-//     0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-//     0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-// };
+static const u8 inv_ascii_table[128] = {
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+       0,    1,    2,    3,    4,    5,    6,    7,
+       8,    9, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff,   10,   11,   12,   13,   14,   15, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff,   10,   11,   12,   13,   14,   15, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+};
 
-// int u8_to_hex(u8 *out, const u8 *in, size_t inlen)
-// {
-//     size_t i;
+int u8_to_hex(u8 *out, const u8 *in, size_t inlen)
+{
+    size_t i;
 
-//     if (out == NULL)
-//         return -1;
+    if (out == NULL)
+        return -1;
 
-//     for (i = 0; i < inlen; i++) {
-//         out[0] = ascii_table[in[i] >> 4];
-//         out[1] = ascii_table[in[i] & 0xf];
-//         out += 2;
-//     }
+    for (i = 0; i < inlen; i++) {
+        out[0] = ascii_table[in[i] >> 4];
+        out[1] = ascii_table[in[i] & 0xf];
+        out += 2;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
-// int hex_to_u8(u8 *out, const u8 *in, size_t inlen)
-// {
-//     size_t i;
+int hex_to_u8(u8 *out, const u8 *in, size_t inlen)
+{
+    size_t i;
 
-//     if (out == NULL)
-//         return -1;
+    if (out == NULL)
+        return -1;
 
-//     i = 0;
-//     if (inlen % 2 == 1) {
-//         out[0] = inv_ascii_table[in[i]];
-//         if (out[0] == 0xff)
-//             return -1;
-//         out++;
-//         i++;
-//     }
+    i = 0;
+    if (inlen % 2 == 1) {
+        out[0] = inv_ascii_table[in[i]];
+        if (out[0] == 0xff)
+            return -1;
+        out++;
+        i++;
+    }
 
-//     for (; i < inlen; i += 2) {
-//         out[0] = (inv_ascii_table[in[i]] << 4) | inv_ascii_table[in[i+1]];
-//         // if (out[0] == 0xff)
-//         //     return FP256_ERR;
-//         out++;
-//     }
-//     return 0;
-// }
+    for (; i < inlen; i += 2) {
+        out[0] = (inv_ascii_table[in[i]] << 4) | inv_ascii_table[in[i+1]];
+        // if (out[0] == 0xff)
+        //     return FP256_ERR;
+        out++;
+    }
+    return 0;
+}
 
-// void print_hex(const char *desp, const u8 *s, size_t slen)
-// {
-//     size_t i;
+void print_hex(const char *desp, const u8 *s, size_t slen)
+{
+    size_t i;
 
-//     for(i = 0; i < strlen(desp); i++)
-//         printf("%c", desp[i]);
+    for(i = 0; i < strlen(desp); i++)
+        printf("%c", desp[i]);
 
-//     u8 *hex = (u8*)malloc(2*slen);
-//     u8_to_hex(hex, s, slen);
-//     for(i = 0; i < 2*slen; i++)
-//         printf("%c", hex[i]);
-//     printf("\n");
-//     free(hex);
-// }
+    u8 *hex = (u8*)malloc(2*slen);
+    u8_to_hex(hex, s, slen);
+    for(i = 0; i < 2*slen; i++)
+        printf("%c", hex[i]);
+    printf("\n");
+    free(hex);
+}
 
-// /* bad rng, but good for test */
-// void random_string(u8 *s, size_t len)
-// {
-//     static int en = 0;
-//     size_t i;
+/* bad rng, but good for test */
+void random_string(u8 *s, size_t len)
+{
+    static int en = 0;
+    size_t i;
 
-//     srand((unsigned)time(NULL) + en);
-//     for (i = 0; i < len; i++)
-//         s[len] = rand() % 256;
-//     en++;
-// }
+    srand((unsigned)time(NULL) + en);
+    for (i = 0; i < len; i++)
+        s[len] = rand() % 256;
+    en++;
+}
 
-// size_t random_number(void)
-// {
-//     static int en = 0;
-//     srand((unsigned)time(NULL) + en);
-//     en++;
-//     return (size_t)rand();
-// }
+size_t random_number(void)
+{
+    static int en = 0;
+    srand((unsigned)time(NULL) + en);
+    en++;
+    return (size_t)rand();
+}
